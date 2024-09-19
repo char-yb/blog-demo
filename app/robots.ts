5672c74cc456767c4d172ced40c0a83db9a39888
ybchar <uiurihappy@sju.ac.kr>
2024-09-19 22:37:31 +0900

    init Commit

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,8 @@
 import { baseUrl } from 'app/sitemap'
+import { MetadataRoute } from 'next'
 
-export default function robots() {
+// 검색 엔진 최적화를 위한 크롤링 봇이다.
+export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {

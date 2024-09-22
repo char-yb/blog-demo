05e5f7f685a4540c0bfcbdf55cc5f262f7567909
ybchar <uiurihappy@sju.ac.kr>
2024-09-22 15:17:06 +0900

    feat: theme toggle

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { ThemeSwitcher } from './theme-switcher'
 
 const navItems = {
   '/': {
@@ -34,6 +35,7 @@ export function Navbar() {
             })}
           </div>
         </nav>
+        <ThemeSwitcher />
       </div>
     </aside>
   )
